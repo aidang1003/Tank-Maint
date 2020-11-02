32e931f525ca722bdab6facfbe54dc27dbd8450e
Gorny <agorny@uiowa.edu>
2020-11-01 20:19:08 -0600

    Fixed form
    Form is now working without app crashes

--- app.py
+++ app.py
@@ -1,7 +1,35 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 
 app = Flask(__name__)
+
+import math
+
+class Tank():
+    def __init__(self, radius, height):
+        self.radius = radius
+        self.height = height
+        
+    def __str__(self):
+        return "Tank with radius {} ft and height {} ft, will cost ${} total".format(self.radius, self.height, self.totalCost())
+        
+    def tankTopArea(self):
+        return math.pi * (self.radius ** 2)
+    
+    def tankSideArea(self):
+        return (2 * (math.pi * self.radius)) * self.height
+    
+    def tankTotalArea(self):
+        return self.tankSideArea() + self.tankTopArea()
+    
+    def materialCost(self):
+        return round(self.tankTotalArea() * 25, 2)
+    
+    def laborCost(self):
+        return round(self.tankTotalArea() * 15, 2)
+    
+    def totalCost(self):
+        return '${0:.2f}'.format(self.materialCost() + self.laborCost())
 
 @app.route('/')
 def index():
@@ -15,8 +43,21 @@ def home():
 def about():
     return render_template('about.html')
 
-@app.route('/estimate')
+@app.route('/estimate', methods=["GET","POST"])
 def estimate():
+    
+    if request.method == "POST":
+        form = request.form
+
+        tRadius = float(form['tRadius'])
+        tHeight = float(form['tHeight'])
+
+        tank = Tank(tRadius, tHeight)
+        tankCost = tank.totalCost()
+        print(tankCost)
+
+        #return render_template('index.html', tankCost=tankCost)
+
     return render_template('estimate.html')
 
 if __name__ == '__main__':
